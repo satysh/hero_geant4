11cbc1dee4f375de09d04e73a98c34286ceb676c
satysh <satyshevi@gmail.com>
2022-05-17 16:30:02 +0300

    add pdg to bind

--- project/output/bindrootfiles.C
+++ project/output/bindrootfiles.C
@@ -7,6 +7,7 @@ void bindrootfiles()
   }
 
   std::vector<Int_t> vEvId;
+  std::vector<Int_t> vPdg;
   std::vector<Double_t> vTime;
   while(!fin.eof()) {
     TString fileName;
@@ -19,6 +20,7 @@ void bindrootfiles()
       return;
     }
     Int_t curEvId;
+    Int_t curPdg;
     Double_t curTime;
     TTree *curTree = (TTree*)file->Get("HERO");
     if (!curTree) {
@@ -26,12 +28,14 @@ void bindrootfiles()
       return;
     }
     curTree->SetBranchAddress("eventId", &curEvId);
+    curTree->SetBranchAddress("pdg", &curPdg);
     curTree->SetBranchAddress("t", &curTime);
     Int_t nEntries = curTree->GetEntries();
     cout << fileName << " has " << nEntries << " entries!" << endl;
     for (Int_t i=0; i<nEntries; i++) {
       curTree->GetEntry(i);
       vEvId.push_back(curEvId);
+      vPdg.push_back(curPdg);
       vTime.push_back(curTime);
     }
     delete curTree;
@@ -40,14 +44,17 @@ void bindrootfiles()
 
   TFile *file = new TFile("hero.root", "RECREATE");
   Int_t evId;
+  Int_t pdg;
   Double_t time;
   TTree *tree = new TTree("bindHERO", "data");
   tree->Branch("eventId", &evId);
+  tree->Branch("pdg", &pdg);
   tree->Branch("time", &time);
 
   cout << "Resulting entries number is " << vEvId.size() << endl;
   for (Int_t i=0; i<vEvId.size(); i++) {
     evId=vEvId[i];
+    pdg=vPdg[i];
     time=vTime[i];
     tree->Fill();
   }
