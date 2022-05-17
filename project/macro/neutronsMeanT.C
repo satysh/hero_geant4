12b2d13fd75c84f8d8f478bdad6c19a740555bab
satysh <satyshevi@gmail.com>
2022-05-17 16:39:11 +0300

    add neutronsMeanT.C

---
+++ project/macro/neutronsMeanT.C
@@ -0,0 +1,53 @@
+void neutronsMeanT()
+{
+  TFile *file = new TFile("../output/hero.root", "READ");
+  if (file->IsZombie()) {
+    cerr << "Can't open input file!" << endl;
+    return;
+  }
+
+  TTree *tree = (TTree*)file->Get("bindHERO");
+  if (!tree) {
+    cerr << "Can't find input tree!" << endl;
+    return;
+  }
+
+  // < evId, vect<time> >
+  std::map<int, std::vector<double>> mapEvNeutron;
+  Int_t evId;
+  Int_t pdg;
+  Double_t time;
+  tree->SetBranchAddress("eventId", &evId);
+  tree->SetBranchAddress("pdg", &pdg);
+  tree->SetBranchAddress("time", &time);
+  Int_t nEntries = tree->GetEntries();
+  cout << "nEntries=" << nEntries << endl;
+  for (Int_t i=0; i<nEntries; i++) {
+    tree->GetEntry(i);
+    //cout << evId << ", " << time << endl;
+    if (pdg == 2112) // only for neutrons
+        mapEvNeutron[evId].push_back(time);
+  }
+
+  file->Close();
+
+  TTree *meanTree = new TTree("meanTree", "meanTree");
+  Int_t meanTime;
+  meanTree->Branch("meanTime", &meanTime);
+  std::map <int, std::vector<double>> :: iterator it = mapEvNeutron.begin();
+  for (; it != mapEvNeutron.end(); it++) {
+    //cout << it->first << ", " << it->second.size() << endl;
+    meanTime = 0;
+    for (Int_t i=0; i<it->second.size(); i++) {
+      meanTime += it->second[i];
+    }
+    meanTime /= it->second.size();
+    //meanTime *= 0.001; // to usec
+    meanTree->Fill();
+  }
+  meanTree->Draw("meanTime*0.001");
+  TH1F *hist = (TH1F*)gROOT->FindObject("htemp");
+  hist->GetXaxis()->SetTitle("mean time moment [usec]");
+  hist->SetTitle("mean time moment for neutrons");
+  gPad->SetGrid(2,2);
+}
