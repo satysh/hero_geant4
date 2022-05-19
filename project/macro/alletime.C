5073a628d03394ada92147ba154e425d1c8beeb2
satysh <satyshevi@gmail.com>
2022-05-19 16:08:40 +0300

    add alletime.C

---
+++ project/macro/alletime.C
@@ -0,0 +1,59 @@
+Bool_t time(TH1F*, Int_t index);
+void alletime()
+{
+  Int_t binN = 125;
+  TH1F *histo = new TH1F("histo", "histo", binN, 0., 1e6/15.);
+
+  for (Int_t i=1; i<27; i++) {
+    if ( !time(histo, i) ) return;
+  }
+
+  histo->Draw();
+  //tree->Draw("time >> histo", "pdg == 1000020040");
+  gPad->SetGrid(2,2);
+  histo->SetTitle("alpha distribution");
+  histo->GetXaxis()->SetTitle("time moment [usec]");
+  histo->SetLineWidth(3);
+  histo->SetLineColor(1);
+
+}
+// ----------------------------------------------------------
+Bool_t time(TH1F *histo, Int_t index)
+{
+  TString inFileName;
+  inFileName.Form("../output/hero_%d.root", index);
+  TFile *file = new TFile(inFileName, "READ");
+  if (file->IsZombie()) {
+    cerr << "Can't open input file " << inFileName << endl;
+    return kFALSE;
+  }
+
+  TTree *tree = (TTree*)file->Get("bindHERO");
+  if (!tree) {
+    cerr << "Can't find input tree!" << endl;
+    return kFALSE;
+  }
+
+  Int_t evId;
+  Int_t pdg;
+  Double_t time;
+  tree->SetBranchAddress("eventId", &evId);
+  tree->SetBranchAddress("pdg", &pdg);
+  tree->SetBranchAddress("time", &time);
+  Int_t nEntries = tree->GetEntries();
+  std::map<int, int> mapEvId;
+  for (Int_t i=0; i<nEntries; i++) {
+    //cout << i << endl;
+    tree->GetEntry(i);
+    mapEvId[evId]++;
+    //cout << evId << ", " << time << endl;
+    // 2112
+    // 1000020040
+    if (pdg == 1000020040) // only for alpha
+      histo->Fill(time*1e-3);
+  }
+  std::map<int, int> :: iterator it=mapEvId.end();
+  cout << "nEvents: " << it->first << ", nEntries=" << nEntries << endl;
+  return kTRUE;
+}
+
