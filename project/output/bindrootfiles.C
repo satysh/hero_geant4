16047978533510f1731c80044144ca1c09ba0f12
satysh <satyshevi@gmail.com>
2022-05-26 17:21:17 +0300

    add radius

--- project/output/bindrootfiles.C
+++ project/output/bindrootfiles.C
@@ -8,6 +8,7 @@ void bindrootfiles(TString outFileName = "hero.root")
   
   std::vector<Int_t> vEvId;
   std::vector<Int_t> vPdg;
+  std::vector<Int_t> vRadius;
   std::vector<Double_t> vTime;
   while(!fin.eof()) {
     TString fileName;
@@ -21,6 +22,9 @@ void bindrootfiles(TString outFileName = "hero.root")
     }
     Int_t curEvId;
     Int_t curPdg;
+    Double_t curX;
+    Double_t curY;
+    Double_t curZ;
     Double_t curTime;
     TTree *curTree = (TTree*)file->Get("HERO");
     if (!curTree) {
@@ -29,6 +33,9 @@ void bindrootfiles(TString outFileName = "hero.root")
     }
     curTree->SetBranchAddress("eventId", &curEvId);
     curTree->SetBranchAddress("pdg", &curPdg);
+    curTree->SetBranchAddress("x", &curX);
+    curTree->SetBranchAddress("y", &curY);
+    curTree->SetBranchAddress("z", &curZ);
     curTree->SetBranchAddress("t", &curTime);
     Int_t nEntries = curTree->GetEntries();
     cout << fileName << " has " << nEntries << " entries!" << endl;
@@ -37,6 +44,8 @@ void bindrootfiles(TString outFileName = "hero.root")
       vEvId.push_back(curEvId);
       vPdg.push_back(curPdg);
       vTime.push_back(curTime);
+      Double_t curRadius = TMath::Sqrt(curX*curX+curY*curY+curZ*curZ);
+      vRadius.push_back(curRadius);
     }
     delete curTree;
     file->Close();
@@ -46,16 +55,19 @@ void bindrootfiles(TString outFileName = "hero.root")
   TFile *file = new TFile(outFileName, "RECREATE");
   Int_t evId;
   Int_t pdg;
+  Double_t radius;
   Double_t time;
   TTree *tree = new TTree("bindHERO", "data");
   tree->Branch("eventId", &evId);
   tree->Branch("pdg", &pdg);
+  tree->Branch("radius", &radius);
   tree->Branch("time", &time);
 
   cout << "Resulting entries number is " << vEvId.size() << endl;
   for (Int_t i=0; i<vEvId.size(); i++) {
     evId=vEvId[i];
     pdg=vPdg[i];
+    radius=vRadius[i];
     time=vTime[i];
     tree->Fill();
   }
