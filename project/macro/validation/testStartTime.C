a426089e04ef40f8b47af939569a3b8d0b6560ea
satysh <satyshevi@gmail.com>
2023-02-21 14:02:25 +0300

    modify test

--- project/macro/validation/testStartTime.C
+++ project/macro/validation/testStartTime.C
@@ -1,24 +1,23 @@
 void testStartTime()
 {
-  TH1F *histo = new TH1F("histo", "histo", 100, 0., (1./16.)*1e9);
-  for (Int_t i=1; i<4; i++) {
-    TString inFileName;
-    inFileName.Form("../output/err_%d.txt", i);
-    fstream fin(inFileName.Data());
-    if (!fin.is_open()) {
-      cerr << "Can't read " << inFileName << endl;
-      return;
-    }
-    while (!fin.eof()) {
-      TString curStr;
-      fin >> curStr;
-      if (curStr == "HEROPrimaryGenerator::GeneratePrimaries_start_time=") {
-        Double_t time;
-        fin >> time;
-        histo->Fill(time);
-      }
-    }
-    fin.close();
+  TH1F *histo = new TH1F("histo", "histo", 100, 0., 1e3+100);
+  TString inFileName = "input/start_times.txt";
+  //inFileName.Form("../output/err_%d.txt", i);
+  fstream fin(inFileName.Data());
+  if (!fin.is_open()) {
+    cerr << "Can't read " << inFileName << endl;
+    return;
   }
+
+  while (true) {
+    string curStr;
+    Double_t time;
+    fin >> curStr >> time; // skip
+    if (fin.eof()) break;
+    histo->Fill(time);
+  }
+  fin.close();
+
+  cout << "nEntries: " << histo->GetEntries() << endl;
   histo->Draw();
 }
