970c3f1e1875e8b58e68df36df3504ad34dee908
satysh <satyshevi@gmail.com>
2023-08-27 16:20:36 +0300

    add proton run

---
+++ project/prun.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+
+if [ -d out_diff_e/ ];then
+    rm -fv out_diff_e/*
+else
+    mkdir out_diff_e
+fi
+
+cd ../build
+make -j3
+wait
+
+particle=2
+energy=10
+nevents=10000
+out_file_name=particle_${particle}_${energy}_GeV_${nevents}_events.root
+
+./hero ${particle} ${energy} ${nevents} ${out_file_name}
+wait
+
+mv ${out_file_name} ../project/out_diff_e
+
+particle=2
+energy=100
+nevents=1000
+out_file_name=particle_${particle}_${energy}_GeV_${nevents}_events.root
+
+./hero ${particle} ${energy} ${nevents} ${out_file_name}
+wait
+
+mv ${out_file_name} ../project/out_diff_e
+
+particle=2
+energy=1000
+nevents=100
+out_file_name=particle_${particle}_${energy}_GeV_${nevents}_events.root
+
+./hero ${particle} ${energy} ${nevents} ${out_file_name}
+wait
+
+mv ${out_file_name} ../project/out_diff_e
+
