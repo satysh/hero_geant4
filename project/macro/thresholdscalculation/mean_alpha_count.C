91f82a5a611cd3f24e96d61e45797605e341adc6
satysh <satyshevi@gmail.com>
2023-09-04 12:58:52 +0300

    add new analisys

---
+++ project/macro/thresholdscalculation/mean_alpha_count.C
@@ -0,0 +1,74 @@
+void mean_alpha_count()
+{
+  TFile* file = new TFile("input/Fe_10_GeV_1000_events.root", "READ");
+  if (file->IsZombie()) {
+    cerr << "Can't read file!" << endl;
+    return;
+  }
+
+  TTree* tree = (TTree*)file->Get("HERO");
+  if (!tree) {
+    cerr << "Can't find tree!" << endl;
+    return;
+  }
+
+  Int_t binN = 50;
+  Double_t minBin = 0.;
+  Double_t maxBin = (Double_t)binN;
+  TH1F* hist = new TH1F("hist", "hist", binN, minBin, maxBin);
+
+  tree->Draw("t*0.001 >> hist", "pdg==1000020040 && t < 128000");
+
+  Int_t nEvents = 1000;
+
+  TH1F* normed_hist = new TH1F("normed_hist", "normed_hist", binN, minBin, maxBin);
+/*
+  TTree* thd_count_tree = new TTree("thd_count_tree", "thd_count_tree");
+  Int_t thd_count;
+  thd_count_tree->Branch("thd_count", &thd_count, "thd_count/I");
+
+  for (Int_t i=0; i<30; i++) {
+    thd_count = hist->GetBinContent(i);
+
+    cout << thd_count << endl;
+    if (thd_count > 0)
+      thd_count_tree->Fill();
+  }
+
+  TCanvas* canv = new TCanvas("canv", "canv");
+  thd_count_tree->Draw("thd_count");
+*/
+
+  TTree* thd_count_tree = new TTree("thd_count_tree", "thd_count_tree");
+  Int_t thd_count;
+  thd_count_tree->Branch("thd_count", &thd_count, "thd_count/I");
+
+  Double_t meanCount = 0.;
+  Double_t max_count = 0.;
+  Double_t sx = 0.;
+  for(Int_t i=0; i<binN; i++) {
+    Int_t now_bin_content = (Int_t)hist->GetBinContent(i) / nEvents;
+    normed_hist->SetBinContent(i, now_bin_content);
+    if (now_bin_content > max_count)
+      max_count = now_bin_content;
+    if (now_bin_content != 0) {
+      sx += (Double_t)(now_bin_content * now_bin_content);
+      meanCount += (Double_t)now_bin_content;
+      thd_count = now_bin_content;
+      thd_count_tree->Fill();
+    }
+  }
+
+  meanCount /= binN;
+  Double_t coeff = 1. / binN / (binN - 1);
+  sx = TMath::Sqrt(coeff * (sx - binN * meanCount * meanCount));
+  cout << "mean=" << meanCount << ", stdDev=" << sx << ", max_count=" << max_count << endl;
+  normed_hist->Draw("SAME");
+  normed_hist->SetLineColor(kGreen);
+  normed_hist->SetLineWidth(3);
+
+
+  TCanvas* canv = new TCanvas("canv", "canv");
+  thd_count_tree->Draw("thd_count");
+
+}
---
+++ project/macro/thresholdscalculation/thd_count_distr.C
@@ -0,0 +1,53 @@
+void thd_count_distr()
+{
+  TFile* file = new TFile("input/128us_2010_2505ev.root", "READ");
+  if (file->IsZombie()) {
+    cerr << "Can't read a file!" << endl;
+    return;
+  }
+
+  TTree* tree = (TTree*)file->Get("HERO");
+  if (!tree) {
+    cerr << "Can't find a tree!" << endl;
+    return;
+  }
+
+  Int_t binN = 160;
+  Double_t binMin = 0.;
+  Double_t binMax = (Double_t)binN;
+  TH1F* thd_h = new TH1F("thd_h", "thd_h", binN, binMin, binMax);
+
+  tree->Draw("t*0.001 >> thd_h", "pdg == 1000020040");
+
+/*
+  TH1F *new_h = new TH1F("normed", "normed", binN, binMin, binMax);
+  new_h->SetTitle("alpha threshold distribution per 1 [usec] in range the [0:128] [usec]");
+  new_h->GetXaxis()->SetTitle("time moment [nanosec]");
+  new_h->GetYaxis()->SetTitle("counts");
+
+  Double_t numOfPrimaryEvents = 2505.;
+  Double_t numOfPrimaryEventsIn1pre16sec = 2505.;
+  Double_t norm = numOfPrimaryEventsIn1pre16sec / numOfPrimaryEvents;
+
+  for (Int_t i=0; i<binN; i++) {
+    Double_t oldBinContent = (Double_t)thd_h->GetBinContent(i);
+    Double_t newBinContent = oldBinContent * norm;
+    new_h->SetBinContent(i, newBinContent);
+  }
+
+  new_h->Draw();
+*/
+
+  TTree* thd_count_tree = new TTree("thd_count_tree", "thd_count_tree");
+  Int_t thd_count;
+  thd_count_tree->Branch("thd_count", &thd_count, "thd_count/I");
+
+  for (Int_t i=11; i<128; i++) {
+    thd_count = thd_h->GetBinContent(i);
+    cout << thd_count << endl;
+    thd_count_tree->Fill();
+  }
+
+  TCanvas* canv = new TCanvas("canv", "canv");
+  thd_count_tree->Draw("thd_count");
+}
