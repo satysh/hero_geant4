d54abc48beee46504bde9aba4a4509d9e2602451
satysh <satyshevi@gmail.com>
2023-09-18 18:44:33 +0300

    set to 16xrun

--- project/mrun.sh
+++ project/mrun.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
-NTHREADS=3
-
 if [ -d ../build ];then
     echo "../build was found!"
     rm -fv ../build/*.root
@@ -16,9 +14,24 @@ fi
 cd ../build
 make -j3
 wait
-for THR in $(seq 1 ${NTHREADS}); do
-    ./hero ${THR} 1> >(tee out_${THR}.txt ) 2> >(tee err_${THR}.txt) &
-done
+
+./hero 1 1>out_1.txt 2>err_1.txt &
+./hero 10 1>out_10.txt 2>err_10.txt &
+./hero 20 1>out_20.txt 2>err_20.txt &
+./hero 30 1>out_30.txt 2>err_30.txt &
+./hero 40 1>out_40.txt 2>err_40.txt &
+./hero 50 1>out_50.txt 2>err_50.txt &
+./hero 60 1>out_60.txt 2>err_60.txt &
+./hero 70 1>out_70.txt 2>err_70.txt &
+./hero 80 1>out_80.txt 2>err_80.txt &
+./hero 90 1>out_90.txt 2>err_90.txt &
+./hero 100 1>out_100.txt 2>err_100.txt &
+./hero 110 1>out_110.txt 2>err_110.txt &
+./hero 120 1>out_120.txt 2>err_120.txt &
+./hero 130 1>out_130.txt 2>err_130.txt &
+./hero 140 1>out_140.txt 2>err_140.txt &
+./hero 150 1>out_150.txt 2>err_150.txt &
+./hero 160 1>out_160.txt 2>err_160.txt &
 wait
 
 cd -
@@ -26,11 +39,7 @@ if [ -d output ];then
     rm -vf output/*.root
     rm -vf output/*.txt
     mv ../build/*.root output/
-    mv ../build/out*.txt output/
-    mv ../build/err*.txt output/
 else
     mkdir output
     mv ../build/*.root output/
-    mv ../build/out.txt output/
-    mv ../build/err.txt output/
 fi
