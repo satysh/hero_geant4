5651f3b6ce376933e9028035c066632abb268453
Ilyas <satyshevi@gmail.com>
2024-03-13 15:33:43 +0300

    add V print

--- project/macro/precalculations/mass.py
+++ project/macro/precalculations/mass.py
@@ -8,6 +8,8 @@ NORM = (4. / 3.) * pi
 print('введите радиус в сантиметрах: ')
 R = int(input())
 
+print('Vdet=', round(NORM * R**3), 'cm3')
+
 # Внутренний слой сцинтилятор
 m_scint = 0.
 m_wolfram = 0.
@@ -16,11 +18,13 @@ r_wolfram = 0.
 for ri in range(R):
 	r_scint = ri + 0.9
 	r_wolfram = ri 
-	m_scint +=  (r_scint**3 - r_wolfram**3) * ro_scint 
-	m_wolfram += ((r_wolfram + 1.)**3 - r_scint**3) * ro_wolfram 
+	m_scint +=  (r_scint**3 - r_wolfram**3)  
+	m_wolfram += ((r_wolfram + 1.)**3 - r_scint**3)
 
-m_scint *= NORM
-m_wolfram *= NORM
+print('Vscint=', round(m_scint * NORM), 'cm3')
+print('Vabsorb=', round(m_wolfram * NORM), 'cm3')
+m_scint *= NORM * ro_scint
+m_wolfram *= NORM * ro_wolfram 
 print('1. масса детектора для случая, когда самый внутренний слой сцинтиллятор:', round((m_scint + m_wolfram) / 10**6, 2), ' [тонн]')
 print('   из них ', round(m_scint / 10**6, 2), ' [тонн] сцинтиллятора и ', round(m_wolfram / 10**6, 2), ' [тонн] вольфрама.')
 
@@ -33,11 +37,14 @@ r_scint = 0.
 for ri in range(R):
 	r_wolfram = ri + 0.1 
 	r_scint = ri
-	m_wolfram += (r_wolfram**3 - r_scint**3) * ro_wolfram 
-	m_scint +=  ((r_scint + 1.)**3 - r_wolfram**3) * ro_scint 
+	m_wolfram += (r_wolfram**3 - r_scint**3)
+	m_scint +=  ((r_scint + 1.)**3 - r_wolfram**3)
 
-m_scint *= NORM
-m_wolfram *= NORM
+
+print('Vscint=', round(m_scint * NORM), 'cm3')
+print('Vabsorb=', round(m_wolfram * NORM), 'cm3')
+m_scint *= NORM * ro_wolfram 
+m_wolfram *= NORM * ro_scint 
 print('2. масса детектора для случая, когда самый внутренний слой вольфрам:', round((m_scint + m_wolfram) / 10**6, 2), ' [тонн]')
 print('   из них ', round(m_scint / 10**6, 2), ' [тонн] сцинтиллятора и ', round(m_wolfram / 10**6, 2), ' [тонн] вольфрама.')
 
