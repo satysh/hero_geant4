5b4c8123828861057b1900450272ad298673fc8a
Ilyas <satyshevi@gmail.com>
2024-04-22 13:27:03 +0300

    add wb and legends

--- project/macro/diff_r/resolution.C
+++ project/macro/diff_r/resolution.C
@@ -7,6 +7,7 @@ void resolution()
 	TCanvas *c = new TCanvas("canv", "canv", 800, 800);
 	c->Divide(2, 2);
 	TGraph *result = new TGraph();
+	TGraph *wb_result = new TGraph();
 	for (UInt_t i=0; i<npoints; i++) {
 		TString now_file_name;
 		now_file_name.Form("output/%d_GeV_sum_edep_distribution_calc.root", points[i]);
@@ -19,7 +20,7 @@ void resolution()
 		Double_t now_mean = now_h->GetMean();
 		Double_t now_std_dev = now_h->GetStdDev();
 
-		cout << points[i] << ": " << now_mean << ", " << now_std_dev << endl;
+		cout << points[i] << ": " << now_mean << ", " << now_std_dev << ", " << now_std_dev / now_mean << endl;
 	    result->AddPoint((Double_t)points[i], now_std_dev / now_mean);
 	    TString hist_title;
 	    hist_title.Form("%d_GeV", points[i]);
@@ -27,22 +28,73 @@ void resolution()
 	    now_h->SetTitle(hist_title + " primary proton energy");
 	    now_h->GetXaxis()->SetTitle("sum_edep [GeV]");
 	    now_h->GetYaxis()->SetTitle("counts");
+	    now_h->SetLineWidth(3);
+	    now_h->SetFillStyle(3008); // 3008 3003
+	    now_h->SetFillColor(kBlue);
 
 	    c->cd(i+1);
 	    now_h->Draw();
+	    now_h->SetStats(0);
+
+		auto legend = new TLegend(0.5,0.4,0.9,0.9);
+		legend->SetTextSize(0.028);
+		TString legendTitle;
+		legendTitle.Form("%d events", (Int_t)now_h->GetEntries());
+		legend->SetHeader(legendTitle);
+		legendTitle.Form("scint + B, mean = %.2f, stdDev = %.2f", now_mean, now_std_dev);
+		legend->AddEntry(now_h, legendTitle);
+
+		now_file_name.Form("output/wb_%d_GeV_sum_edep_distribution_calc.root", points[i]);
+		now_h = hist(now_file_name);
+		if (!now_h) {
+			continue;
+		}
+
+		now_mean = now_h->GetMean();
+		now_std_dev = now_h->GetStdDev();
+
+		cout << "wb: " << points[i] << ": " << now_mean << ", " << now_std_dev << ", " << now_std_dev / now_mean << endl;
+	    wb_result->AddPoint((Double_t)points[i], now_std_dev / now_mean);
+
+	    legendTitle.Form("scint + only, mean = %.2f, stdDev = %.2f", now_mean, now_std_dev);
+		legend->AddEntry(now_h, legendTitle);
+		now_h->Draw("SAME");
+		now_h->SetLineColor(kRed);
+		now_h->SetLineWidth(3);
+	    now_h->SetFillStyle(3003); // 3008 3003
+	    now_h->SetFillColor(kRed);
+	    hist_title.Form("wb_%d_GeV", points[i]);
+	    now_h->SetName(hist_title);
+
+		legend->Draw();
 	}
 
 	TCanvas *c_res = new TCanvas("c_res", "c_res");
 	c_res->cd();
-	result->Draw("ALP");
 	result->SetMarkerStyle(20);
 	result->SetMarkerSize(2);
 	result->SetMarkerColor(kBlue);
 	result->SetLineColor(kBlue);
 	result->SetLineWidth(3);
-	result->GetXaxis()->SetTitle("E [GeV]");
-	result->GetYaxis()->SetTitle("resolution []");
-	result->SetTitle("resolution as function of primary proton Energy");
+    
+    wb_result->SetMarkerStyle(20);
+	wb_result->SetMarkerSize(2);
+	wb_result->SetMarkerColor(kRed);
+	wb_result->SetLineColor(kRed);
+	wb_result->SetLineWidth(3);
+	
+	auto legend = new TLegend(0.6,0.8,0.75,0.9);
+	legend->AddEntry(result, "scint + B");
+	legend->AddEntry(wb_result, "scint only");
+	
+	TMultiGraph *mg = new TMultiGraph();
+	mg->SetTitle("resolution as function of primary proton Energy");
+	mg->Add(result);
+	mg->Add(wb_result);
+	mg->GetXaxis()->SetTitle("E [GeV]");
+	mg->GetYaxis()->SetTitle("resolution []");
+	mg->Draw("ALP");
+	legend->Draw();
 	gPad->SetGrid(2, 2);
 }
 
@@ -55,7 +107,7 @@ TH1F *hist(TString file_name)
 	}
 
 	auto keys = file->GetListOfKeys();
-	keys->Print(); 
+	//keys->Print(); 
 	TIter iter(keys);
 	TString now_hist_name(iter.Next()->GetName());
 	auto result = (TH1F*)file->Get(now_hist_name);
