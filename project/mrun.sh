4ef9fa05bba19c8b44300f58f631db932cef305c
Ilyas <satyshevi@gmail.com>
2024-05-26 16:40:59 +0300

    set 110 batches by 1 event

--- project/mrun.sh
+++ project/mrun.sh
@@ -2,7 +2,7 @@
 
 # ./tmp.sh ENERGY NEVENTS_IN_RUN NTHREADS NBATCH
 # result NEVENTS= NEVENTS_IN_RUN * NTHREADS * NBATCH
-./tmp.sh 20000 5 16 22
-./tmp.sh 40000 5 16 22			
-./tmp.sh 60000 5 16 22
-./tmp.sh 80000 5 16 22 
+./tmp.sh 20000 1 16 110
+./tmp.sh 40000 1 16 110			
+./tmp.sh 60000 1 16 110
+./tmp.sh 80000 1 16 110
