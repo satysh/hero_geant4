842d35ff08ec90d41c547543ee357ab629f05315
Ilyas <satyshevi@gmail.com>
2024-06-11 15:51:04 +0300

    set 1 simple event mc

--- project/mrun.sh
+++ project/mrun.sh
@@ -3,7 +3,7 @@
 # ./tmp.sh ENERGY NEVENTS_IN_RUN NTHREADS NBATCH SEED_BEGIN
 # result NEVENTS= NEVENTS_IN_RUN * NTHREADS * NBATCH
 # SEED_BEGIN is a number of last event. By def. It is 0.
-./tmp.sh 20000 1 16 110 0
-./tmp.sh 40000 1 16 110	0		
-./tmp.sh 60000 1 16 110 0
-./tmp.sh 80000 1 16 110 0
+./tmp.sh 1 1 1 1 0
+#./tmp.sh 40000 1 16 110	0		
+#./tmp.sh 60000 1 16 110 0
+#./tmp.sh 80000 1 16 110 0
